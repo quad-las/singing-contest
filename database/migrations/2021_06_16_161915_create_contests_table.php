8b930058d46cae9174866fa84cc0fe5b29506fc5
Quadri Lasisi <quadri.lasisi@aboutyou.com>
2021-06-16 18:38:28 +0200

    contest: model, migration, controller

---
+++ database/migrations/2021_06_16_161915_create_contests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contests', function (Blueprint $table) {
+            $table->id();
+            $table->string('winner')->nullable();
+            $table->integer('winning_score')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contests');
+    }
+}
